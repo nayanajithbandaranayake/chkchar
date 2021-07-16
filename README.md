cc52cdd29f370bda5cc3599f543269acac5a18a3
Nayanajith Bandaranayake <hbmnbandaranayake9988033@gmail.com>
2021-07-16 12:42:43 +0530

    Upgraded to version 1.1.0 Added new methods

--- README.md
+++ README.md
@@ -7,26 +7,119 @@ This is a utility package. You can use this to check the availibility of capital
 1. caps(phrase)
 2. numbers(phrase)
 3. symbols(phrase)
-4. isEnglish(phrase)
+4. fullCount(phrase)
+5. isEnglish(phrase)
+6. allNums(phrase)
+7. allSymbols(phrase)
+8. allCaps(phrase)
 
-#### Output formats
+#### Installation
 
-All three methods caps(), numbers(), symbols() bear a equal output format
+> npm i chkchars
+
+#### 1.caps(phrase)
+
+Checks if there are capital english letters in a given string.
+phrase: string
+
+##### Output format
 
 > {
 
     status: true / false    --- true === contains   false === does not contain
-    count: number           --- number of numbers, symbols or caps in the given phrase
-    fount: array            --- all the numbers, symbols or caps found in the given phrase (no duplicates)
+    count: number           --- number of caps in the given phrase
+    fount: array            --- all the caps found in the given phrase (no duplicates)
 
 }
 
-isEnglish method return a boolean value
+#### 2.numbers(phrase)
 
-If all the characters are english
+Checks if there are numbers in a given string.
+phrase: string
+
+##### Output format
+
+> {
+
+    status: true / false    --- true === contains   false === does not contain
+    count: number           --- number of numbers in the given phrase
+    fount: array            --- all the numbers found in the given phrase (no duplicates)
+
+}
+
+#### 3.symbols(phrase)
+
+Checks if there are symbols in a given string.
+phrase: string
+
+##### Output format
+
+> {
+
+    status: true / false    --- true === contains   false === does not contain
+    count: number           --- number of symbols in the given phrase
+    fount: array            --- all the symbols found in the given phrase (no duplicates)
+
+}
+
+#### 4.fullCount(phrase)
+
+Returns the number of occurences of all caps, numbers and symbols
+
+##### Output format
+
+> {
+
+    caps:       --- number of english capital letters found.
+    numbers:    --- number of numbers found.
+    symbols:    --- number of symbols found.
+
+}
+
+#### 5.isEnglish(phrase)
+
+Checks if the given string is composed only with english letters.
+
+If all the characters are english letters
 
 > true
 
-If at least one of them are not english
+If at least one of them not a english letter
+
+> false
+
+#### 6.allNums(phrase)
+
+Checks if the given string is composed only with numbers.
+
+If all the characters are numbers
+
+> true
+
+If at least one of them not a number
+
+> false
+
+#### 7.allSymbols(phrase)
+
+Checks if the given string is composed only with symbols.
+
+If all the characters are symbols
+
+> true
+
+If at least one of them not a symbol
+
+> false
+
+#### 8.allCaps
+
+Checks if the given string is composed only with english capital letters.
+
+If all the characters are english capital letters
+
+> true
+
+If at least one of them not a english capital letter
 
 > false
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const caps = [
+const checkValid = (phrase) => {
+  if (typeof phrase !== "string")
+    throw new Error("Please provide a string to chkchar.");
+};
+
+const caps_values = [
   "A",
   "B",
   "C",
@@ -27,14 +32,48 @@ const caps = [
   "Z",
 ];
 
-module.exports.caps = (phrase) => {
-  if (typeof phrase !== "string")
-    throw new Error("Please provide a string to chkchar.");
+const numbers_values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const symbols_values = [
+  "`",
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "+",
+  "=",
+  "[",
+  "]",
+  "{",
+  "}",
+  "/",
+  "?",
+  "'",
+  '"',
+  ":",
+  ";",
+  ">",
+  "<",
+  ".",
+  ",",
+  "|",
+  "£",
+];
 
+module.exports.caps = (phrase) => {
+  checkValid(phrase);
   let cap_count = 0;
   let found = [];
   for (let char of phrase) {
-    for (let cap of caps) {
+    for (let cap of caps_values) {
       if (char === cap) {
         cap_count++;
         if (!found.includes(cap)) found.push(cap);
@@ -56,13 +95,11 @@ module.exports.caps = (phrase) => {
   }
 };
 module.exports.numbers = (phrase) => {
-  if (typeof phrase !== "string")
-    throw new Error("Please provide a string to chkchar.");
-  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  checkValid(phrase);
   let number_count = 0;
   let found = [];
   for (let char of phrase) {
-    for (let number of numbers) {
+    for (let number of numbers_values) {
       if (char === number) {
         number_count++;
         if (!found.includes(number)) {
@@ -86,46 +123,11 @@ module.exports.numbers = (phrase) => {
   }
 };
 module.exports.symbols = (phrase) => {
-  if (typeof phrase !== "string")
-    throw new Error("Please provide a string to chkchar.");
-  const symbols = [
-    "`",
-    "~",
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")",
-    "-",
-    "_",
-    "+",
-    "=",
-    "[",
-    "]",
-    "{",
-    "}",
-    "/",
-    "?",
-    "'",
-    '"',
-    ":",
-    ";",
-    ">",
-    "<",
-    ".",
-    ",",
-    "|",
-    "£",
-  ];
+  checkValid(phrase);
   let sign_count = 0;
   let found = [];
   for (let char of phrase) {
-    for (let sign of symbols) {
+    for (let sign of symbols_values) {
       if (char === sign) {
         sign_count++;
         if (!found.includes(sign)) found.push(sign);
@@ -148,13 +150,55 @@ module.exports.symbols = (phrase) => {
 };
 
 module.exports.isEnglish = (phrase) => {
-  if (typeof phrase !== "string")
-    throw new Error("Please provide a string to chkchar.");
+  checkValid(phrase);
   const converted = phrase.toUpperCase();
   for (let char of converted) {
-    for (let cap of caps) {
+    for (let cap of caps_values) {
       if (char !== cap) return false;
     }
   }
   return true;
+};
+
+module.exports.allNums = (phrase) => {
+  checkValid(phrase);
+  for (let char of phrase) {
+    for (let num of numbers_values) {
+      if (char !== num) return false;
+    }
+  }
+  return true;
+};
+
+module.exports.allSymbols = (phrase) => {
+  checkValid(phrase);
+  for (let char of phrase) {
+    for (let sign of symbols_values) {
+      if (char !== sign) return false;
+    }
+  }
+  return true;
+};
+
+module.exports.allCaps = (phrase) => {
+  checkValid(phrase);
+  for (let char of phrase) {
+    for (let cap of caps_values) {
+      if (char !== cap) return false;
+    }
+  }
+  return true;
+};
+
+module.exports.fullCount = (phrase) => {
+  checkValid(phrase);
+  const { count: cap_count } = this.caps(phrase);
+  const { count: num_count } = this.numbers(phrase);
+  const { count: sign_count } = this.symbols(phrase);
+
+  return {
+    caps: cap_count,
+    numbers: num_count,
+    symbols: sign_count,
+  };
 };
